715e37c58419808c0cb352d56f203a8ce09c2114
charizard8888 <omkumr082001@outlook.com>
2018-08-05 00:12:21 +0530

    Update abilities.js

--- mods/fe/abilities.js
+++ mods/fe/abilities.js
@@ -949,13 +949,13 @@ exports.BattleAbilities = {
 			}
 		},
 		onUpdate: function(pokemon) {
-			if (pokemon.hp === pokemon.maxhp && pokemon.status === 'brn' || pokemon.status === 'frz' || pokemon.status === 'psn' || pokemon.status === 'tox' || pokemon.status === 'par') {
+			if (pokemon.status === 'brn' || pokemon.status === 'frz' || pokemon.status === 'psn' || pokemon.status === 'tox' || pokemon.status === 'par' && pokemon.hp === pokemon.maxhp) {
 				this.add('-activate', pokemon, 'ability: Pristine');
 				pokemon.cureStatus();
 			}
 		},
 		onImmunity: function(type, pokemon) {
-			if (pokemon.hp === pokemon.maxhp && pokemon.status === 'brn' || pokemon.status === 'frz' || pokemon.status === 'psn' || pokemon.status === 'tox' || pokemon.status === 'par') return false;
+			if (pokemon.status === 'brn' || pokemon.status === 'frz' || pokemon.status === 'psn' || pokemon.status === 'tox' || pokemon.status === 'par' && pokemon.hp === pokemon.maxhp) return false;
 		},
 		id: "pristine",
 		name: "Pristine",
