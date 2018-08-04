548e055e80e7095b8b97ace6c0999afd3ffa177f
charizard8888 <omkumr082001@outlook.com>
2018-08-04 21:25:27 +0530

    Update petmods.js

--- chat-plugins/petmods.js
+++ chat-plugins/petmods.js
@@ -201,18 +201,6 @@ learnistor: function(target, room, user) {
 		this.sendReplyBox(`${buf}</div>`);
 	},
 	eeveedhelp: ["/eeveed - Shows the list of Pokemon in Eeevee'd."],
-	eeveed2: function (target, room, user) {
-		if (!this.runBroadcast()) return;
-		let buf = `<div class=infobox-limited><center><h2>List Of Eeeved Pokemon</h2></center>`;
-		let jillianDex = require('../mods/eeveed/pokedex.js').BattlePokedex;
-		if (!jillianDex) return this.errorReply("Error Fetching Istor Data.");
-		Object.values(jillianDex).forEach(mon => {
-			buf += `<table><tr><td>Pokemon</td><td>Type #1</td><td>Type #2</td><td>Ability #1</td><td>Ability #2</td><td>Ability #3</td></td><td>HP</td><td>Atk</td><td>Def</td><td>SpA</td><td>SpD</td><td>Spe</td></tr><tr><td>${mon.species}</td><td>${mon.types[0]}</td><td>${mon.types[1]}</td><td>${mon.abilities[0]}</td></table>`;
-													 //<td>${mon.abilities.H}</td><td>${mon.baseStats.hp}</td><td>${mon.baseStats.atk}</td><td>${mon.baseStats.def}</td><td>${mon.baseStats.spa}</td><td>${mon.baseStats.spd}</td><td>${mon.baseStats.spe}</td></table>`;
-		});
-		this.sendReplyBox(`${buf}</div>`);
-	},
-	eeveed2help: ["/eeveed - Shows the list of Pokemon in Eeevee'd."],
 	eeveedabilities: function (target, room, user) {
 		if (!this.runBroadcast()) return;
 		let buf = `<div class=infobox-limited><center><h2>List Of Coded Eeveed Abilities</h2></center>`;
