a38d75d565e56551b2e16bc58c39e4923bcbb236
charizard8888 <omkumr082001@outlook.com>
2018-09-03 17:12:00 +0530

    Update formats.js

--- config/formats.js
+++ config/formats.js
@@ -4261,7 +4261,7 @@ exports.Formats = [
   		       "&bullet; <a href=http://www.smogon.com/forums/threads/fusion-moves-fusion-evolution-companion-project.3564805/>Fusion Moves</a>",
   		      ],
   		ruleset: ['Extreme Speed Clause', 'Pokemon', 'Sleep Clause Mod', 'Species Clause', 'Moody Clause', 'Evasion Moves Clause', 'Endless Battle Clause', 'HP Percentage Mod', 'Cancel Mod', 'Team Preview', 'Swagger Clause', 'Baton Pass Clause'],
-		//banlist: ['Unreleased', 'Illegal', 'Dialcatty', 'Kars', 'Dittsey', 'Diceus', 'Peridot-Mega', 'Kyzor', 'Gonzap', 'Harem', 'Cinshado', 'Enteon', 'Mega Lucashadow', 'Kahoot', 'Taiwan', 'Passcraft', 'Dad', 'Enteon', 'Entir', 'Ultra Necrynx', 'Ultra Necrozerain', 'Shenala', 'Mega Xurkizard Y', 'Mega Archedactyl', 'Miminja', 'Mega Toxicario', 'Mega Lucasol L', 'Mega Alakario L', 'Mega Khangorus-Khan', 'Mega Kasukabe', 'Mega Absoko', 'Mega Kartaria', 'Dio', 'Mendoza', 'Deoxurk-Outlet', 'Omneus','Muddy Seed'],
+		banlist: ['Unreleased', 'Illegal', 'Ignore Illegal Moves', 'Dialcatty', 'Kars', 'Dittsey', 'Diceus', 'Peridot-Mega', 'Kyzor', 'Gonzap', 'Harem', 'Cinshado', 'Enteon', 'Mega Lucashadow', 'Kahoot', 'Taiwan', 'Passcraft', 'Dad', 'Enteon', 'Entir', 'Ultra Necrynx', 'Ultra Necrozerain', 'Shenala', 'Mega Xurkizard Y', 'Mega Archedactyl', 'Miminja', 'Mega Toxicario', 'Mega Lucasol L', 'Mega Alakario L', 'Mega Khangorus-Khan', 'Mega Kasukabe', 'Mega Absoko', 'Mega Kartaria', 'Dio', 'Mendoza', 'Deoxurk-Outlet', 'Omneus','Muddy Seed'],
 
 		mod: 'fe',
 		onPrepareHit: function(target, source, move) {
