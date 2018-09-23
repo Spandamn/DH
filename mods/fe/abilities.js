f3c4ed7c00f0d9b3a24d478baf4f3e422bfca19e
KirbyRider1337 <39121695+KirbyRider1337@users.noreply.github.com>
2018-09-23 17:52:26 -0400

    Update abilities.js

--- mods/fe/abilities.js
+++ mods/fe/abilities.js
@@ -11673,12 +11673,7 @@ exports.BattleAbilities = {
  
     "ailmentmaster": {
         shortDesc: "This Pokemon can inflict any status on any other Pokemon regardless of their typing.",
-		  onAnySetStatus: function (status, target, source, effect) {
-			   //Foe must not have a status, and the one who inflicted it must be the one with this ability. 
-				if (target.status || source !== this.effectData.target) return;
-				source.setStatus(status, source, effect, true);
-				return null; 
-		  },
+		  //Implemented in pokemon.js.
         id: "ailmentmaster",
         name: "Ailment Master",
     },
