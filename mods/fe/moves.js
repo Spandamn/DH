76186148d78a7efce0196c4c7cdc9bdd282188aa
charizard8888 <omkumr082001@outlook.com>
2018-09-24 12:38:32 +0530

    Update moves.js

--- mods/fe/moves.js
+++ mods/fe/moves.js
@@ -9413,7 +9413,7 @@ exports.BattleMovedex = {
 		id: "oxygenbuster",
 		isViable: true,
 		name: "Oxygen Buster",
-		pp: 16,
+		pp: 10,
 		priority: 0,
 		flags: {protect: 1, mirror: 1},
 		onEffectiveness: function (typeMod, type) {
