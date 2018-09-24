a55adb72ac61ea5ad830b95863649964516cae31
KirbyRider1337 <39121695+KirbyRider1337@users.noreply.github.com>
2018-09-24 13:33:05 -0400

    Update pokemon.js

--- mods/fe/pokemon.js
+++ mods/fe/pokemon.js
@@ -1189,7 +1189,7 @@ class Pokemon {
 			return false;
 		}
 
-		if (!ignoreImmunities && status.id && !(source && (((source.hasAbility('corrosion') || source.hasAbility('poisonpores')) && ['tox', 'psn'].includes(status.id)) || source.hasAbility('ailmentmaster'))) && !(sourceEffect && sourceEffect.effectType === 'Move' && sourceEffect.id === 'thundervirus')) {
+		if (!ignoreImmunities && status.id && !(source && (source.hasAbility('ailmentmaster') || ((source.hasAbility('corrosion') || source.hasAbility('poisonpores')) && ['tox', 'psn'].includes(status.id)))) && !(sourceEffect && sourceEffect.effectType === 'Move' && sourceEffect.id === 'thundervirus')) {
 			// the game currently never ignores immunities
 			if (!this.runStatusImmunity(status.id === 'tox' ? 'psn' : status.id)) {
 				this.battle.debug('immune to status');
