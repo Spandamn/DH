aed9db465e60377bd367581b71d89b446907d778
Spandamn <spandan.punwatkar@gmail.com>
2018-10-26 21:37:07 +0530

    How do i do this

--- config/formats.js
+++ config/formats.js
@@ -7250,7 +7250,7 @@ exports.Formats = [
 			let itemExists = this.dex.getItem(set.item) || this.dex.getType(set.item) || this.dex.getAbility(set.item) || this.dex.getMove(set.item) || set.item === '';
 			if (!itemExists) return [`You have entered gibberish in the item slot on ${set.name || set.species}.`];
 			let validator = new this.constructor(Dex.getFormat(this.format.id, ['Ignore Illegal Abilities']));
-			let problems = validator.validateSet(Object.assign({}, set, {ability: ''}), teamHas) || validator.validateSet(Object.assign({}, set, {ability: '', item: set.ability}, teamHas)) || [];
+			let problems = validator.validateSet(Object.assign({}, set, {ability: set.item, item: ''}), teamHas) || validator.validateSet(Object.assign({}, set, {ability: '', item: set.ability}, teamHas)) || [];
 			if (set.ability === set.item) problems.push(`You cannot have two of the same thing on a Pokemon. (${set.name || set.species} has two of ${item.name})`);
 			return problems;
 		},
