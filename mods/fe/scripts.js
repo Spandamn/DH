9a5340a9b47e344b613f80ed015ed74318f826ea
KirbyRider1337 <39121695+KirbyRider1337@users.noreply.github.com>
2018-10-28 12:58:28 -0400

    Update scripts.js

--- mods/fe/scripts.js
+++ mods/fe/scripts.js
@@ -109,7 +109,7 @@ exports.BattleScripts = {
 				let ppDrop = this.runEvent('DeductPP', source, pokemon, move);
 				if (ppDrop !== true) {
 					extraPP += ppDrop || 0;
-					if (ppDrop && pokemon.hasAbility('powerdrain') && source.runStatusImmunity('par', false)){
+					if (ppDrop && pokemon.hasAbility('powerdrain') && source.runStatusImmunity('par', false) && !source.status){
 						this.add('-ability', pokemon, 'Power Drain');
 						source.trySetStatus('par', pokemon);
 					}
