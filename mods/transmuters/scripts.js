a97a307f5aa279b51846291597742ed66f71d750
Spandamn <spandan.punwatkar@gmail.com>
2018-11-06 20:37:21 +0530

    Fix Transmuters

--- mods/transmuters/scripts.js
+++ mods/transmuters/scripts.js
@@ -1,6 +1,6 @@
 'use strict';
 
-exports.BattleScripts = {
+let BattleScripts = {
 	getItem: function (name) {
 		let pokemon = this.getTemplate(name);
 		return pokemon.exists ? Object.assign(pokemon, {transmuteAbility: pokemon.abilities[0]}) : Object.getPrototypeOf(this).getItem.call(this, name);
@@ -65,87 +65,90 @@ exports.BattleScripts = {
 
 	pokemon: {
 		formeChange: function (templateId, source = this.battle.effect, isPermanent, message, abilitySlot = '0') {
-		let rawTemplate = this.battle.getTemplate(templateId);
+			let rawTemplate = this.battle.getTemplate(templateId);
 
-		if (!rawTemplate.abilities) return false;
+			if (!rawTemplate.abilities) return false;
 
-		let template = this.battle.singleEvent('ModifyTemplate', this.battle.getFormat(), null, this, source, null, rawTemplate);
+			let template = this.battle.singleEvent('ModifyTemplate', this.battle.getFormat(), null, this, source, null, rawTemplate);
 
-		if (!template) return false;
+			if (!template) return false;
 
-		this.template = template;
+			this.template = template;
 
-		this.setType(template.types, true);
-		this.apparentType = rawTemplate.types.join('/');
-		this.addedType = template.addedType || '';
-		this.knownType = true;
-		if (this.battle.gen >= 7) this.removeVolatile('autotomize');
+			this.setType(template.types, true);
+			this.apparentType = rawTemplate.types.join('/');
+			this.addedType = template.addedType || '';
+			this.knownType = true;
+			if (this.battle.gen >= 7) this.removeVolatile('autotomize');
 
-		if (source) {
-			let stats = this.battle.spreadModify(this.template.baseStats, this.set);
-			if (!this.baseStats) this.baseStats = stats;
-			for (let statName in this.stats) {
-				// @ts-ignore
-				this.stats[statName] = stats[statName];
-				// @ts-ignore
-				this.baseStats[statName] = stats[statName];
-				// @ts-ignore
-				if (this.modifiedStats) this.modifiedStats[statName] = stats[statName]; // Gen 1: Reset modified stats.
-			}
-			if (this.battle.gen <= 1) {
-				// Gen 1: Re-Apply burn and para drops.
-				// FIXME: modifyStat() is only defined for the Gen 1 mod...
-				// @ts-ignore
-				if (this.status === 'par') this.modifyStat('spe', 0.25);
-				// @ts-ignore
-				if (this.status === 'brn') this.modifyStat('atk', 0.5);
-			}
-			this.speed = this.stats.spe;
-			if ((!source.id && !source.effectType) || this.battle.gen <= 2) return true;
-
-			let apparentSpecies = this.illusion ? this.illusion.template.species : template.baseSpecies; // The species the opponent sees
-			if (isPermanent) {
-				this.baseTemplate = rawTemplate;
-				this.details = template.species + (this.level === 100 ? '' : ', L' + this.level) + (this.gender === '' ? '' : ', ' + this.gender) + (this.set.shiny ? ', shiny' : '');
-				this.battle.add('detailschange', this, (this.illusion || this).details);
-				if (source.effectType === 'Item') {
+			if (source) {
+				let stats = this.battle.spreadModify(this.template.baseStats, this.set);
+				if (!this.baseStats) this.baseStats = stats;
+				for (let statName in this.stats) {
 					// @ts-ignore
-					if (source.zMove) {
-						this.battle.add('-burst', this, apparentSpecies, template.requiredItem);
-						this.moveThisTurnResult = true; // Ultra Burst counts as an action for Truant
-					} else if (source.onPrimal) {
-						if (this.illusion) {
-							this.ability = '';
-							this.battle.add('-primal', this.illusion);
+					this.stats[statName] = stats[statName];
+					// @ts-ignore
+					this.baseStats[statName] = stats[statName];
+					// @ts-ignore
+					if (this.modifiedStats) this.modifiedStats[statName] = stats[statName]; // Gen 1: Reset modified stats.
+				}
+				if (this.battle.gen <= 1) {
+					// Gen 1: Re-Apply burn and para drops.
+					// FIXME: modifyStat() is only defined for the Gen 1 mod...
+					// @ts-ignore
+					if (this.status === 'par') this.modifyStat('spe', 0.25);
+					// @ts-ignore
+					if (this.status === 'brn') this.modifyStat('atk', 0.5);
+				}
+				this.speed = this.stats.spe;
+				if ((!source.id && !source.effectType) || this.battle.gen <= 2) return true;
+
+				let apparentSpecies = this.illusion ? this.illusion.template.species : template.baseSpecies; // The species the opponent sees
+				if (isPermanent) {
+					this.baseTemplate = rawTemplate;
+					this.details = template.species + (this.level === 100 ? '' : ', L' + this.level) + (this.gender === '' ? '' : ', ' + this.gender) + (this.set.shiny ? ', shiny' : '');
+					this.battle.add('detailschange', this, (this.illusion || this).details);
+					if (source.effectType === 'Item') {
+						// @ts-ignore
+						if (source.zMove) {
+							this.battle.add('-burst', this, apparentSpecies, template.requiredItem);
+							this.moveThisTurnResult = true; // Ultra Burst counts as an action for Truant
+						} else if (source.onPrimal) {
+							if (this.illusion) {
+								this.ability = '';
+								this.battle.add('-primal', this.illusion);
+							} else {
+								this.battle.add('-primal', this);
+							}
+						} else if (source.transmuteAbility) {
+							this.battle.add('-transmute', this, apparentSpecies, source.transmuteAbility);
+							this.moveThisTurnResult = true; // Transmutation counts as an action for Truant
 						} else {
-							this.battle.add('-primal', this);
+							this.battle.add('-mega', this, apparentSpecies, template.requiredItem);
+							this.moveThisTurnResult = true; // Mega Evolution counts as an action for Truant
 						}
-					} else if (source.transmuteAbility) {
-						this.battle.add('-transmute', this, apparentSpecies, source.transmuteAbility);
-						this.moveThisTurnResult = true; // Transmutation counts as an action for Truant
-					} else {
-						this.battle.add('-mega', this, apparentSpecies, template.requiredItem);
-						this.moveThisTurnResult = true; // Mega Evolution counts as an action for Truant
+					} else if (source.effectType === 'Status') {
+						// Shaymin-Sky -> Shaymin
+						this.battle.add('-formechange', this, template.species, message);
 					}
-				} else if (source.effectType === 'Status') {
-					// Shaymin-Sky -> Shaymin
-					this.battle.add('-formechange', this, template.species, message);
-				}
-			} else {
-				if (source.effectType === 'Ability') {
-					this.battle.add('-formechange', this, template.species, message, `[from] ability: ${source.name}`);
 				} else {
-					this.battle.add('-formechange', this, this.illusion ? this.illusion.template.species : template.species, message);
+					if (source.effectType === 'Ability') {
+						this.battle.add('-formechange', this, template.species, message, `[from] ability: ${source.name}`);
+					} else {
+						this.battle.add('-formechange', this, this.illusion ? this.illusion.template.species : template.species, message);
+					}
+				}
+				if (source.effectType !== 'Ability' && source.id !== 'relicsong' && source.id !== 'zenmode') {
+					if (this.illusion) {
+						this.ability = ''; // Don't allow Illusion to wear off
+					}
+					this.setAbility(template.abilities[abilitySlot], null, true);
+					if (isPermanent) this.baseAbility = this.ability;
 				}
 			}
-			if (source.effectType !== 'Ability' && source.id !== 'relicsong' && source.id !== 'zenmode') {
-				if (this.illusion) {
-					this.ability = ''; // Don't allow Illusion to wear off
-				}
-				this.setAbility(template.abilities[abilitySlot], null, true);
-				if (isPermanent) this.baseAbility = this.ability;
-			}
-		}
-		return true;
-	}
+			return true;
+		},
+	},
 };
+
+exports.BattleScripts = BattleScripts;
