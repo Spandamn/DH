e5ec9f84d2e18ef75619d53d6867ecbb90293913
charizard8888 <omkumr082001@outlook.com>
2018-12-09 19:46:07 +0530

    Update pokedex.js

--- mods/ccam/pokedex.js
+++ mods/ccam/pokedex.js
@@ -14,6 +14,34 @@ let BattlePokedex = {
     inherit: true,
     abilities: {0: "Levitate", H: "Time Chime"},
     },
+	porygonz: {
+    inherit: true,
+    abilities: {0: "Adaptability", 1: "Cheat Code", H: "Analytic"},
+    },
+	celebi: {
+    inherit: true,
+    abilities: {0: "Natural Cure", H: "Magician"},
+    },
+	inkay: {
+    inherit: true,
+    abilities: {0: "Contrary", 1: "Suction Cups", H: "Backmask"},
+    },
+	malamar: {
+    inherit: true,
+    abilities: {0: "Contrary", 1: "Suction Cups", H: "Backmask"},
+    },
+	meloetta: {
+    inherit: true,
+    abilities: {0: "Serene Grace", H: "Backmask"},
+    },
+	smoochum: {
+    inherit: true,
+    abilities: {0: "Backmask", 1: "Forewarn", H: "Hydration"},
+    },
+	jynx: {
+    inherit: true,
+    abilities: {0: "Backmask", 1: "Forewarn", H: "Hydration"},
+    },
 };
 
 exports.BattlePokedex = BattlePokedex;
