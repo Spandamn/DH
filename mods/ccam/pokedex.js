e8044977aff453ca44a6c35933afd3c66f402410
charizard8888 <omkumr082001@outlook.com>
2018-12-09 19:31:45 +0530

    Create pokedex.js

---
+++ mods/ccam/pokedex.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**@type {{[k: string]: TemplateData}} */
+let BattlePokedex = {
+  bronzong: {
+    inherit: true,
+    abilities: {0: "Levitate", 1: "Heatproof", H: "Time Chime"},
+    },
+  chingling: {
+    inherit: true,
+    abilities: {0: "Levitate", H: "Time Chime"},
+    },
+   chimecho: {
+    inherit: true,
+    abilities: {0: "Levitate", H: "Time Chime"},
+    },
+};
+
+exports.BattlePokedex = BattlePokedex;
