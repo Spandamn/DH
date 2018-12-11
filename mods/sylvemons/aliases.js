8f24bf43fea061874ba0acddfab5c433150599d6
Spandamn <spandan.punwatkar@gmail.com>
2018-12-11 23:41:10 +0530

    Change Sylvemons a bit

--- mods/sylvemons/aliases.js
+++ mods/sylvemons/aliases.js
@@ -3,11 +3,11 @@
 /**@type {{[k: string]: string}} */
 let BattleAliases = {
 	// basically deerling & sawsbuck formes
-	"deerlingspring": "Deerling",
+	"deerlingspring": "Deerling-Spring",
 	"deerlingsummer": "Deerling-Summer",
 	"deerlingautumn": "Deerling-Autumn",
 	"deerlingwinter": "Deerling-Winter",
-	"sawsbuckspring": "Sawsbuck",
+	"sawsbuckspring": "Sawsbuck-Spring",
 	"sawsbucksummer": "Sawsbuck-Summer",
 	"sawsbuckautumn": "Sawsbuck-Autmn",
 	"sawsbuckwinter": "Sawsbuck-Winter",
