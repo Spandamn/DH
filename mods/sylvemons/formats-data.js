b02b74fe970d6d6e037826d068d42e1b4955467e
Spandamn <spandan.punwatkar@gmail.com>
2018-12-11 23:38:19 +0530

    Change Sylvemons a bit

--- mods/sylvemons/formats-data.js
+++ mods/sylvemons/formats-data.js
@@ -1,13 +1,53 @@
 'use strict';
 
 let BattleFormatsData = {
-	sawsbuck: {
-		inherit: true,
-		isNonstandard: true,
+	sawsbuckautumn: {
+		randomBattleMoves: ["swordsdance", "hornleech", "jumpkick", "return", "substitute"],
+		randomDoubleBattleMoves: ["swordsdance", "hornleech", "jumpkick", "return", "substitute", "synthesis", "protect"],
+		tier: "SYM",
+		doublesTier: "DUU",
 	},
-	deerling: {
-		inherit: true,
-		isNonstandard: true,
+	sawsbuckwinter: {
+		randomBattleMoves: ["swordsdance", "hornleech", "jumpkick", "return", "substitute"],
+		randomDoubleBattleMoves: ["swordsdance", "hornleech", "jumpkick", "return", "substitute", "synthesis", "protect"],
+		tier: "SYM",
+		doublesTier: "DUU",
+	},
+	sawsbucksummer: {
+		randomBattleMoves: ["swordsdance", "hornleech", "jumpkick", "return", "substitute"],
+		randomDoubleBattleMoves: ["swordsdance", "hornleech", "jumpkick", "return", "substitute", "synthesis", "protect"],
+		tier: "SYM",
+		doublesTier: "DUU",
+	},
+	sawsbuckspring: {
+		randomBattleMoves: ["swordsdance", "hornleech", "jumpkick", "return", "substitute"],
+		randomDoubleBattleMoves: ["swordsdance", "hornleech", "jumpkick", "return", "substitute", "synthesis", "protect"],
+		tier: "SYM",
+		doublesTier: "DUU",
+	},
+	deerlingautumn: {
+		randomBattleMoves: ["swordsdance", "hornleech", "jumpkick", "return", "substitute"],
+		randomDoubleBattleMoves: ["swordsdance", "hornleech", "jumpkick", "return", "substitute", "synthesis", "protect"],
+		tier: "SYM",
+		doublesTier: "DUU",
+	},
+	deerlingwinter: {
+		randomBattleMoves: ["swordsdance", "hornleech", "jumpkick", "return", "substitute"],
+		randomDoubleBattleMoves: ["swordsdance", "hornleech", "jumpkick", "return", "substitute", "synthesis", "protect"],
+		tier: "SYM",
+		doublesTier: "DUU",
+	},
+	deerlingsummer: {
+		randomBattleMoves: ["swordsdance", "hornleech", "jumpkick", "return", "substitute"],
+		randomDoubleBattleMoves: ["swordsdance", "hornleech", "jumpkick", "return", "substitute", "synthesis", "protect"],
+		tier: "SYM",
+		doublesTier: "DUU",
+	},
+	deerlingspring: {
+		randomBattleMoves: ["swordsdance", "hornleech", "jumpkick", "return", "substitute"],
+		randomDoubleBattleMoves: ["swordsdance", "hornleech", "jumpkick", "return", "substitute", "synthesis", "protect"],
+		tier: "SYM",
+		doublesTier: "DUU",
 	},
 };
 exports.BattleFormatsData =  BattleFormatsData;
