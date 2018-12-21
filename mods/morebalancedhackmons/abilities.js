3fec16d9a5eb626bae899759645985bd06c5e3ba
Brodaha <30511274+Brodaha@users.noreply.github.com>
2018-12-21 13:54:39 -0700

    Update abilities.js

--- mods/morebalancedhackmons/abilities.js
+++ mods/morebalancedhackmons/abilities.js
@@ -3,10 +3,10 @@
 /**@type {{[k: string]: AbilityData}} */
 let BattleAbilities = {
 "fluffycloak": {
-		shortDesc: "This Pokemon's Sp. Defense is doubled.",
+		shortDesc: "This Pokemon's Sp. Defense is 1.5x.",
 		onModifyDefPriority: 6,
 		onModifyDef: function (spd) {
-			return this.chainModify(2);
+			return this.chainModify(1.5);
 		},
 		id: "fluffycloak",
 		name: "Fluffy Cloak",
@@ -58,6 +58,48 @@ let BattleAbilities = {
 		},
 		id: "gravitate",
 		name: "Gravitate",
+	},
+	"magician": {
+		shortDesc: "On switch-in, this Pokemon summons Magic Room.",
+		onStart: function(source) {
+			this.useMove("Magic Room", source);
+		},
+		id: "magician",
+		name: "Magician",
+	},
+	"scrappy": {
+		shortDesc: "This Pokemon can hit Steel types with Poison-type moves.",
+		onModifyMovePriority: -5,
+		onModifyMove: function (move) {
+			if (!move.ignoreImmunity) move.ignoreImmunity = {};
+			if (move.ignoreImmunity !== true) {
+				move.ignoreImmunity['Poison'] = true;
+			}
+		},
+		id: "scrappy",
+		name: "Scrappy",
+		rating: 3,
+		num: 113,
+	},
+	"illuminate": {
+		desc: "If a Pokemon uses a Ghost- or Dark-type attack against this Pokemon, that Pokemon's attacking stat is halved when calculating the damage to this Pokemon.",
+		shortDesc: "Ghost/Dark-type moves against this Pokemon deal damage with a halved attacking stat.",
+		onModifyAtkPriority: 6,
+		onSourceModifyAtk: function (atk, attacker, defender, move) {
+			if (move.type === 'Dark' || move.type === 'Ghost') {
+				this.debug('Illuminate weaken');
+				return this.chainModify(0.5);
+			}
+		},
+		onModifySpAPriority: 5,
+		onSourceModifySpA: function (atk, attacker, defender, move) {
+			if (move.type === 'Dark' || move.type === 'Ghost') {
+				this.debug('Illuminate weaken');
+				return this.chainModify(0.5);
+			}
+		},
+		id: "illuminate",
+		name: "Illuminate",
 	},
   };
 
