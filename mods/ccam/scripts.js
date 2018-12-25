869e5de0fad7e65f528819f40a49d9917497da4f
charizard8888 <omkumr082001@outlook.com>
2018-12-25 15:37:33 +0530

    Create scripts.js

---
+++ mods/ccam/scripts.js
@@ -0,0 +1,9 @@
+'use strict';
+
+exports.BattleScripts = {
+  init: function() {
+  this.modData('Learnsets', 'darmanitan').learnset.slackoff = ['7L1'];
+  this.modData('Learnsets', 'sawsbuck').learnset.seasonalstrike = ['7L1'];
+  this.modData('Learnsets', 'kyuremblack').learnset.freezedry = ['7L1'];
+  },
+  };
