e64cd3f778863f2c03166db6807558e78e036744
charizard8888 <omkumr082001@outlook.com>
2018-12-25 15:16:21 +0530

    Update moves.js

--- mods/dhssb/moves.js
+++ mods/dhssb/moves.js
@@ -13,6 +13,11 @@ exports.BattleMovedex = {
 		pp: 5,
 		priority: 0,
 		flags: {contact: 1, protect: 1, mirror: 1},
+		selfBoost: {
+			boosts: {
+				spe: 1,
+			},
+		},
 		secondary: null,
 		target: "normal",
 		type: "Fire",
