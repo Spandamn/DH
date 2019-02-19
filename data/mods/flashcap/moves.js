1e181502f19dc9ddb7b95fd91db108def1dcf1cf
Brodaha <30511274+Brodaha@users.noreply.github.com>
2019-02-19 14:41:53 -0700

    Update moves.js

--- data/mods/flashcap/moves.js
+++ data/mods/flashcap/moves.js
@@ -20,6 +20,27 @@ let BattleMovedex = {
 		zMovePower: 100,
 		contestType: "Cool",
 	},
+"stingjab": {
+		num: 410,
+		accuracy: 100,
+		basePower: 85,
+		category: "Physical",
+		desc: "30% chance to paralyze.",
+		shortDesc: "30% chance to paralyze.",
+		id: "stingjab",
+		name: "Sting Jab",
+		pp: 15,
+		priority: 0,
+		flags: {protect: 1, mirror: 1},
+		secondary: {
+			chance: 30,
+			status: 'par',
+		},
+		target: "normal",
+		type: "Poison",
+		zMovePower: 160,
+		contestType: "Cool",
+	},
 };
 
 exports.BattleMovedex = BattleMovedex;
