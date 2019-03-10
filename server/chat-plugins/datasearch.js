fa3ebd57bcc8f8af7afdb8f58f398c666e034ef2
Spandamn <spandan.punwatkar@gmail.com>
2019-03-10 14:34:22 +0530

    Make Tweak to Datasearch command

--- server/chat-plugins/datasearch.js
+++ server/chat-plugins/datasearch.js
@@ -281,6 +281,7 @@ function runDexsearch(target, cmd, canAll, message) {
 	let allEggGroups = {'amorphous': 'Amorphous', 'bug': 'Bug', 'ditto': 'Ditto', 'dragon': 'Dragon', 'fairy': 'Fairy', 'field': 'Field', 'flying': 'Flying', 'grass': 'Grass', 'humanlike': 'Human-Like', 'mineral': 'Mineral', 'monster': 'Monster', 'undiscovered': 'Undiscovered', 'water1': 'Water 1', 'water2': 'Water 2', 'water3': 'Water 3', __proto__: null};
 	let allStats = ['hp', 'atk', 'def', 'spa', 'spd', 'spe', 'bst', 'weight', 'height', 'gen'];
 	let showAll = false;
+	let order = null;
 	let megaSearch = null;
 	let capSearch = null;
 	let randomOutput = 0;
@@ -423,6 +424,29 @@ function runDexsearch(target, cmd, canAll, message) {
 				if (!canAll) return {reply: "A search with the parameter 'all' cannot be broadcast."};
 				if (parameters.length > 1) return {reply: "The parameter 'all' cannot have alternative parameters"};
 				showAll = true;
+				orGroup.skip = true;
+				break;
+			}
+
+			if (target.substr(0, 3) === 'asc' || target.substr(0, 3) === 'des') {
+				if (parameters.length > 1) return {reply: `The parameter '${target.substr(0, 3)}' cannot have alternative parameters`};
+				let stat = target.split(' ')[1];
+				if (!allStats.includes(stat)) return {reply: `'${escapeHTML(target)}' did not contain a valid stat.`};
+				switch (toId(stat)) {
+				case 'attack': stat = 'atk'; break;
+				case 'defense': stat = 'def'; break;
+				case 'specialattack': stat = 'spa'; break;
+				case 'spc': stat = 'spa'; break;
+				case 'special': stat = 'spa'; break;
+				case 'spatk': stat = 'spa'; break;
+				case 'specialdefense': stat = 'spd'; break;
+				case 'spdef': stat = 'spd'; break;
+				case 'speed': stat = 'spe'; break;
+				case 'wt': stat = 'weight'; break;
+				case 'ht': stat = 'height'; break;
+				case 'generation': stat = 'gen'; break;
+				}
+				order = `${target.substr(0, 3) === 'asc' ? '+' : '-'}${stat}`;
 				orGroup.skip = true;
 				break;
 			}
@@ -593,6 +617,19 @@ function runDexsearch(target, cmd, canAll, message) {
 	// Prioritize searches with the least alternatives.
 	const accumulateKeyCount = (count, searchData) => count + (typeof searchData === 'object' ? Object.keys(searchData).length : 0);
 	searches.sort((a, b) => Object.values(a).reduce(accumulateKeyCount, 0) - Object.values(b).reduce(accumulateKeyCount, 0));
+	if (order) {
+		let stat = order.substr(1);
+		let sort = order[0];
+		searches.sort((a, b) => {
+			if (!(stat in a.baseStats)) {
+				if (sort === '+') return a[stat] - b[stat];
+				if (sort === '-') return b[stat] - a[stat];
+			} else {
+				if (sort === '+') return a.baseStats[stat] - b.baseStats[stat];
+				if (sort === '-') return b.baseStats[stat] - a.baseStats[stat];
+			}
+		})
+	}
 
 	let lsetData = {};
 	for (const alts of searches) {
