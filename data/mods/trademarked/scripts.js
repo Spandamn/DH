49335a5375b602d4e326af1d71bb47f7f3d151da
Spandamn <spandan.punwatkar@gmail.com>
2019-03-10 13:17:35 +0530

    Update Trademarked

--- data/mods/trademarked/scripts.js
+++ data/mods/trademarked/scripts.js
@@ -4,7 +4,7 @@ exports.BattleScripts = {
 	init: function() {
 		Object.values(this.data.Movedex).forEach(move => {
 			let bannedMoves = ['Assist', 'Baneful Bunker', 'Block', 'Copycat', 'Detect', 'Mat Block', 'Mean Look', 'Nature Power', 'Protect', 'Roar', 'Spider Web', 'Spiky Shield', 'Whirlwind'];
-			if (move.category === 'Status' && !bannedMoves.includes(move.name) && !(move.status && move.status === slp)) {
+			if (move.category === 'Status' && !bannedMoves.includes(move.name) && !(move.status && move.status === 'slp')) {
 				this.data.Abilities[move.id] = {
 					desc: move.desc,
 					shortDesc: move.shortDesc,
