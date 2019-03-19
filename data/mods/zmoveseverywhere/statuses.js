2232fc65f5b5daf9d67f94e2e7ff85382750c169
charizard8888 <omkumr082001@outlook.com>
2019-03-19 13:25:35 +0530

    Update statuses.js

--- data/mods/zmoveseverywhere/statuses.js
+++ data/mods/zmoveseverywhere/statuses.js
@@ -60,6 +60,38 @@ let BattleStatuses = {
 			if (this.effectData.source && this.effectData.source.isActive) pokemon.tryTrap();
 		},
 	},
+	slp: {
+		name: 'slp',
+		id: 'slp',
+		num: 0,
+		effectType: 'Status',
+		onStart(target, source, sourceEffect) {
+			if (sourceEffect && sourceEffect.effectType === 'Ability') {
+				this.add('-status', target, 'slp', '[from] ability: ' + sourceEffect.name, '[of] ' + source);
+			} else {
+				this.add('-status', target, 'slp');
+			}
+			// 1-3 turns
+			this.effectData.startTime = this.random(2, 5);
+			this.effectData.time = this.effectData.startTime;
+		},
+		onBeforeMovePriority: 10,
+		onBeforeMove(pokemon, target, move) {
+			if (pokemon.hasAbility('earlybird') || pokemon.volatiles['lovelylullaby']) {
+				pokemon.statusData.time--;
+			}
+			pokemon.statusData.time--;
+			if (pokemon.statusData.time <= 0) {
+				pokemon.cureStatus();
+				return;
+			}
+			this.add('cant', pokemon, 'slp');
+			if (move.sleepUsable) {
+				return;
+			}
+			return false;
+		},
+	},
 
 };
 
