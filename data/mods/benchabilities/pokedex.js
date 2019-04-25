2164b6cfcc73e5785e12ba53288a9c3ab6cc0431
charizard8888 <omkumr082001@outlook.com>
2019-04-25 12:51:11 +0530

    Update pokedex.js

--- data/mods/benchabilities/pokedex.js
+++ data/mods/benchabilities/pokedex.js
@@ -5,6 +5,7 @@ let BattlePokedex = {
 victini: {
 		inherit: true,
 		//abilities: {0: "Victory Star", S: "Confidence Boost"},
+	// https://github.com/Spandamn/DH/blob/master/data/mods/benchabilities/scripts.js#L31
 	},
 };
 
