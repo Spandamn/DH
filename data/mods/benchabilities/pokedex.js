6514374006097e9bd150a10243c7566731956368
charizard8888 <omkumr082001@outlook.com>
2019-04-25 12:22:31 +0530

    Update pokedex.js

--- data/mods/benchabilities/pokedex.js
+++ data/mods/benchabilities/pokedex.js
@@ -3,16 +3,8 @@
 /**@type {{[k: string]: TemplateData}} */
 let BattlePokedex = {
 victini: {
-		num: 494,
-		species: "Victini",
-		types: ["Psychic", "Fire"],
-		gender: "N",
-		baseStats: {hp: 100, atk: 100, def: 100, spa: 100, spd: 100, spe: 100},
-		abilities: {0: "Victory Star", B: "Confidence Boost"},
-		heightm: 0.4,
-		weightkg: 4,
-		color: "Yellow",
-		eggGroups: ["Undiscovered"],
+		inherit: true,
+		abilities: {0: "Victory Star", S: "Confidence Boost"},
 	},
 };
 
