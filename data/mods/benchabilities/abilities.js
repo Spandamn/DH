c63d98cdac2c5a3507a6c41d03fe8e7680be2cd4
charizard8888 <omkumr082001@outlook.com>
2019-04-25 13:05:42 +0530

    Update abilities.js

--- data/mods/benchabilities/abilities.js
+++ data/mods/benchabilities/abilities.js
@@ -22,13 +22,13 @@ let BattleAbilities = {
 				let bestStat = 0;
 				/** @type {StatNameExceptHP} */
 				let s;
-				for (s in source.storedStats) {
-					if (source.storedStats[s] > bestStat) {
+				for (s in target.storedStats) {
+					if (target.storedStats[s] > bestStat) {
 						statName = s;
-						bestStat = source.storedStats[s];
+						bestStat = target.storedStats[s];
 					}
 				}
-				this.boost({[statName]: 1}, source);
+				this.boost({[statName]: 1}, target);
 			}
 		},
 		id: "confidenceboost",
