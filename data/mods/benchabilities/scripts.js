5309f0020a3ec7ec8ca32bf65c07b1d2fb46c64b
charizard8888 <omkumr082001@outlook.com>
2019-04-25 13:25:12 +0530

    Update scripts.js

--- data/mods/benchabilities/scripts.js
+++ data/mods/benchabilities/scripts.js
@@ -50,5 +50,14 @@ exports.BattleScripts = {
 		this.modData('Pokedex', 'rotomfrost').abilities['S'] = 'Arctic Armor';
 		this.modData('Pokedex', 'kyurem').abilities['S'] = 'Arctic Armor';
 		
+		// Heavy Expert: Onix, Steelix, Rhyhorn, Rhydon, Rhyperior, Aron, Lairon, Aggron			
+		this.modData('Pokedex', 'onix').abilities['S'] = 'Heavy Expert';
+		this.modData('Pokedex', 'steelix').abilities['S'] = 'Heavy Expert';
+		this.modData('Pokedex', 'shyhorn').abilities['S'] = 'Heavy Expert';
+		this.modData('Pokedex', 'rhydon').abilities['S'] = 'Heavy Expert';
+		this.modData('Pokedex', 'rhyperior').abilities['S'] = 'Heavy Expert';
+		this.modData('Pokedex', 'aron').abilities['S'] = 'Heavy Expert';
+		this.modData('Pokedex', 'lairon').abilities['S'] = 'Heavy Expert';
+		this.modData('Pokedex', 'aggron').abilities['S'] = 'Heavy Expert';
 	}
 };
