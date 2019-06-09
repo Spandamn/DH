cf591b3d4c5f8c891d5c92f231a8845785223a86
Spandamn <spandan.punwatkar@gmail.com>
2019-06-09 14:12:51 +0530

    Add Secret Slot

--- config/formats.js
+++ config/formats.js
@@ -6753,8 +6753,8 @@ exports.Formats = [
 				for (let i = 0; i < side.pokemon.length; i++) {
 					let pokemon = side.pokemon[i];
 					let prevPoke = side.pokemon[i === 0 ? side.pokemon.length - 1 : i - 1];
-					let move = this.getMove(Object.assign({}, prevPoke.baseMoveSlots[prevPoke.baseMoveSlots.length - 1]).id);
-					pokemon.baseMoveSlots.push({
+					let move = this.getMove(prevPoke.moves[prevPoke.moves.length - 1]);
+					pokemon.moveSlots.push({
 						move: move.name,
 						id: move.id,
 						pp: ((move.noPPBoosts || move.isZ) ? move.pp : move.pp * 8 / 5),
